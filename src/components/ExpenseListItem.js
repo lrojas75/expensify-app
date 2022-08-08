1b568f8406da49de5c3e47ab50e68c2107855ae5
Luis <luis@paladin.insure>
2022-08-07 20:46:35 -0500

    Added summary component

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -11,7 +11,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => {
         <h3>{description}</h3>
       </NavLink>
       <p>
-        {numeral(amount / 100).format("$0,0.00")} -
+        {numeral(amount / 100).format("$0,0.00")}-
         {moment(createdAt).format("MMMM Do YYYY")}
       </p>
       <button onClick={() => navigate(`/edit/${id}`)}>Edit</button>
